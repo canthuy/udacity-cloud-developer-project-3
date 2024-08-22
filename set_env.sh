de5374d98b306311431d54c857d8f9ce03ace478
Can Thuy <canthuy140499@gmail.com>
2024-08-22 23:08:57 +0700

    add project

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=123456789
+export POSTGRES_HOST=cloud-developer-project-3.c8rvoqkrvypi.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udacity-project-3
+export AWS_BUCKET=arn:aws:s3:::udacity-project-3-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
