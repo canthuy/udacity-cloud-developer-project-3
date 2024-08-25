036a43ae716b85a935608e383aeb8df0ff494eb6
Can Thuy <canthuy140499@gmail.com>
2024-08-25 12:39:44 +0700

    update

--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -24,14 +24,25 @@ import { V0_USER_MODELS } from './controllers/v0/model.index';
   // We set the CORS origin to * so that we don't need to
   // worry about the complexities of CORS this lesson. It's
   // something that will be covered in the next course.
-  app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', config.url);
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    next();
-  });
+  app.use(
+    cors({
+      allowedHeaders: [
+        'Origin',
+        'X-Requested-With',
+        'Content-Type',
+        'Accept',
+        'X-Access-Token',
+        'Authorization',
+        'Access-Control-Allow-Origin',
+        'Access-Control-Allow-Headers',
+        'Access-Control-Allow-Methods',
+      ],
+      methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+      preflightContinue: true,
+      origin: '*',
+      credentials: true,
+    })
+  );
 
   app.use('/api/v0/', IndexRouter);
 
